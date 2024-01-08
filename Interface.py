28bb3fbbb8ae1c3597bb06715cb1652218e1c607
Leo BERNARD <leobernard712@gmail.com>
2024-01-08 16:07:21 +0100

    amélioration de l'interface
    ajout d'un text scrollable avec retour à la ligne sans couper les mots.
    ajout d'une animation de chargement lors du traitement en thread de la recherche

--- Interface.py
+++ Interface.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+import tkinter.scrolledtext as st
 from tkinter import ttk
 import Main
 import sys
+import threading
 
 # Création de la fenêtre principale
 root = tk.Tk()
@@ -15,25 +17,51 @@ class PrintCapture:
     def write(self, text):
         self.text_widget.insert(tk.END, text)
 
-def clique_bouton():
+def animation_chargement(i=0):
+    global continuer_animation
+    if continuer_animation:
+        chargement_label.config(text="Chargement" + "." * i)
+        root.update()
+        if chargement_label.cget("text") == "Chargement...":
+            i = 0
+        else:
+            i += 1
+        root.after(250, animation_chargement, i)
+
+def recherche_thread():
+    global continuer_animation
     topic = topic_entry.get()
     mots_cles = mots_cles_entry.get()
     nombre_articles = nombre_articles_spinbox.get()
     methode_affichage = methode_affichage_combobox.get()
-    
-    # Supprimer le contenu de resultat_text
-    resultat_text.delete(1.0, tk.END)
-
-    # Redirect print statements to the text widget
-    sys.stdout = PrintCapture(resultat_text)
 
     # Appel de la fonction recherche et affichage du résultat de la recherche
     resultat = Main.recherche(topic, mots_cles, nombre_articles, methode_affichage)
 
-    # Restore the standard output
+    # Restaurer les sorties standard
     sys.stdout = sys.__stdout__
 
     resultat_text.insert(tk.END, resultat)
+
+    # Arrêter l'animation de chargement
+    continuer_animation = False
+    chargement_label.config(text="")
+
+def clique_bouton():
+    global continuer_animation
+
+    # Démarrer l'animation de chargement
+    continuer_animation = True
+    animation_chargement()
+
+    # Supprimer le contenu de resultat_text
+    resultat_text.delete(1.0, tk.END)
+
+    # Redirection des sorties standard (print) vers resultat_text
+    sys.stdout = PrintCapture(resultat_text)
+
+    # Lancer la recherche dans un autre thread
+    threading.Thread(target=recherche_thread).start()
 
 # Widgets pour saisir le topic de la recherche
 topic_label = ttk.Label(root, text="Topic")
@@ -55,23 +83,31 @@ methode_affichage_label = ttk.Label(root, text="Méthode d'affichage")
 methode_affichage_combobox = ttk.Combobox(root, values=['Pertinence', 'Temporelle', 'Alphabétique'])
 methode_affichage_combobox.set('Pertinence')
 
+# Créer un label pour le message de chargement
+chargement_label = ttk.Label(root, text="", foreground="red")
+# Variable pour contrôler si l'animation doit continuer
+continuer_animation = False
+
 # Widget pour afficher le résultat de la recherche
-resultat_text = tk.Text(root)
+resultat_text = st.ScrolledText(root, width=120, height=35, wrap=tk.WORD)
 
 # Widget pour lancer la recherche
 bouton_recherche = ttk.Button(root, text="Rechercher", command=clique_bouton)
 
+
+
 # Placement des widgets en utilisant la grille
-topic_label.grid(row=0, column=0, sticky="w", padx=50)
-topic_entry.grid(row=0, column=1, padx=5, pady=5)
-mots_cles_label.grid(row=1, column=0, sticky="w", padx=50)
-mots_cles_entry.grid(row=1, column=1, padx=5, pady=5)
-nombre_articles_label.grid(row=2, column=0, sticky="w", padx=50)
-nombre_articles_spinbox.grid(row=2, column=1, padx=5, pady=5)
-methode_affichage_label.grid(row=3, column=0, sticky="w", padx=50)
-methode_affichage_combobox.grid(row=3, column=1, padx=5, pady=5)
-bouton_recherche.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
-resultat_text.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+topic_label.grid(row=0, column=0, sticky="E", padx=10, pady=5)
+topic_entry.grid(row=0, column=1, sticky="w", padx=10, pady=5)
+mots_cles_label.grid(row=1, column=0, sticky="E", padx=10, pady=5)
+mots_cles_entry.grid(row=1, column=1, sticky="w", padx=10, pady=5)
+nombre_articles_label.grid(row=2, column=0, sticky="E", padx=10, pady=5)
+nombre_articles_spinbox.grid(row=2, column=1, sticky="w", padx=10, pady=5)
+methode_affichage_label.grid(row=3, column=0, sticky="E", padx=10, pady=5)
+methode_affichage_combobox.grid(row=3, column=1, sticky="w", padx=10, pady=5)
+bouton_recherche.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
+chargement_label.grid(row=5, column=0, columnspan=2)
+resultat_text.grid(row=6, column=0, columnspan=2, padx=10, pady=5)
 
 # Lancement de la boucle principale
 root.mainloop()
